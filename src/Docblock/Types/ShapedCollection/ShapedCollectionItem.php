7eacb027130c8be7f435f1ce078ef3b773b97410
Remco Smits <djsmits12@gmail.com>
2022-10-23 00:01:27 +0200

    Remove debug code

--- src/Docblock/Types/ShapedCollection/ShapedCollectionItem.php
+++ src/Docblock/Types/ShapedCollection/ShapedCollectionItem.php
@@ -41,26 +41,3 @@ final class ShapedCollectionItem
         return $this->type;
     }
 }
-
-//array<int,
-//    array1{
-//        remco: string|int,
-//        normal: array2{
-//                    t: string,
-//                    a: array{
-//                        h: string,
-//                        t: mixed
-//                    }
-//        },
-//        a: string,
-//        1: mixed,
-//        array: array3{
-//                age: int,
-//                name: string,
-//                hobbies: array4{
-//                            0: string,
-//                            1?: string|int
-//                }
-//        }
-//    }
-//>
